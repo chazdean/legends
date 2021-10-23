07b9e71e3f475b791211cc46793331a8b38b7aff
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-23 01:18:11 +0000

    refactor register POST route, tidy and remove console logs

--- routes/login.js
+++ routes/login.js
@@ -20,8 +20,7 @@ module.exports = (db) => {
   });
 
   router.post("/login", (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+    const { email, password } = req.body;
 
     // Checking first for empty fields
     if (!email || !password) {
@@ -50,9 +49,7 @@ module.exports = (db) => {
   });
 
   router.post("/register", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const password = req.body.password;
+    const { name, email, password } = req.body;
 
     // Checking first for empty fields
     if (!name || !email || !password) {
@@ -61,27 +58,26 @@ module.exports = (db) => {
 
     // Check if the user already exists in the db
     getUserWithEmail(email, db)
-    .then(data => {
-      if (data) {
-        return res.render("login", { error: "This email already exists, please login!" });
-      }
+      .then(data => {
+        if (data) {
+          return res.render("login", { error: "This email already exists, please login!" });
+        }
 
-      // If the user is new, add user to the db and log them in by setting the session-cookie
-      addNewUser(req.body, db)
-        .then((userData) => {
-          console.log('returned new userData', userData);
-          req.session.user_id = userData.id;
-          res.redirect("/maps");
-        })
-        .catch(e => {
-          console.error(e);
-          res.send(e);
-        });
-    })
-    .catch(e => {
-      console.error(e);
-      res.send(e);
-    });
+        // If the user is new, add user to the db and log them in by setting the session-cookie
+        addNewUser(req.body, db)
+          .then((userData) => {
+            req.session.user_id = userData.id;
+            res.redirect("/maps");
+          })
+          .catch(e => {
+            console.error(e);
+            res.send(e);
+          });
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   router.post("/logout", (req, res) => {
