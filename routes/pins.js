88b6acbbaf99924457792b033a725378d2821c82
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-23 22:52:38 +0000

    add first get route to display pins page

--- routes/pins.js
+++ routes/pins.js
@@ -4,13 +4,25 @@
 
 const express = require('express');
 const router  = express.Router();
+const { getUserWithId } = require('../databaseHelpers/userQueries');
+const { getPinsForMap } = require('../databaseHelpers/pinsQueries');
+
 
 module.exports = (db) => {
 
   //Render the map details page with list of pins and the google API map
-  router.get("/pins/:map_id", (req, res) => {
+  router.get("/:map_id", (req, res) => {
+    const map_id = req.params.map_id;
 
-    res.render("pins");
+    getPinsForMap(map_id, db)
+      .then((pinsArray) => {
+        console.log(pinsArray);
+        res.render("pins");
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   return router;
