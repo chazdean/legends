0b0246c7ea40893b38644690ff0d182404486685
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-23 22:51:49 +0000

    add first query to select pins for a given map_id

--- databaseHelpers/pinsQueries.js
+++ databaseHelpers/pinsQueries.js
@@ -1,8 +1,29 @@
 // Helpers for pins database queries
 
+const getPinsForMap = function(map_id, db) {
+  const queryParams = [map_id];
+  const queryString = `
+    SELECT
+      pins.*, users.name AS creator
+    FROM
+      pins
+      JOIN users ON pins.creator_id = users.id
+    WHERE
+      pins.map_id = $1
+    ORDER BY
+      pins.title;`;
 
+  return db.query(queryString, queryParams)
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+
+};
 
 
 module.exports = {
-
+  getPinsForMap
 };
