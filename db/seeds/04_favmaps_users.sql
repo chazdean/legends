e7d93614824fb53dcb6cbbb6ac71125db4a6cc95
chazdean <chaz.dean@shopify.com>
2021-10-24 19:29:33 +0000

    add map routes for main page, mymaps, and favourite maps

--- db/seeds/04_favmaps_users.sql
+++ db/seeds/04_favmaps_users.sql
@@ -1,21 +1,21 @@
 -- seeds for favmaps_users table
 
 INSERT INTO favmaps_users
-  (user_id, map_id)
+  (user_id, map_id, active)
 VALUES
-  (1, 2),
-  (1, 3),
-  (2, 1),
-  (2, 2),
-  (2, 3),
-  (3, 2),
-  (3, 1),
-  (3, 4),
-  (4, 4),
-  (6, 1),
-  (6, 2),
-  (7, 3),
-  (8, 3),
-  (8, 2),
-  (9, 1),
-  (9, 4);
+  (1, 2, true),
+  (1, 3, true),
+  (2, 1, true),
+  (2, 2, true),
+  (2, 3, true),
+  (3, 2, true),
+  (3, 1, true),
+  (3, 4, true),
+  (4, 4, true),
+  (6, 1, true),
+  (6, 2, true),
+  (7, 3, true),
+  (8, 3, true),
+  (8, 2, true),
+  (9, 1, true),
+  (9, 4, true);
--- routes/maps.js
+++ routes/maps.js
@@ -5,52 +5,65 @@
 const express = require('express');
 const { func } = require('joi');
 const router  = express.Router();
+const {
+  getMaps,
+  getUserMaps,
+  getFavouriteMaps
+} = require('../databaseHelpers/mapsQueries')
 
 module.exports = (db) => {
 
   //Render the main maps page
   router.get("/", (req, res) => {
     // database functions to query for a list of 20 maps
-
-    const getMaps = function(limit) {
-    const sql = `
-      SELECT *
-      FROM maps
-      LIMIT $1
-      `;
-    const params = [limit]
-
-     return db.query(sql, params)
-        .then(res => res.rows)
-        .catch(err => console.log(err.message));
-    }
-
-    getMaps(20).then((maps) => {
-      console.log("ALL MAPS", maps)
-      res.render("maps");
-    });
+    getMaps(db)
+      .then(result => {
+        console.log(result)
+        console.log(req.session.user_id)
+        res.render("maps", { maps: result, user: req.session.user_id })
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   //Render individual users maps
   router.get("/:user_id", (req, res) => {
     // database functions to query for all individual user maps
     // based on user_id
-
-    res.render("maps");
+    getUserMaps(req.params['user_id'], db)
+      .then(result => {
+        console.log(result)
+        res.render("maps", { maps: result, user: req.session.user_id });
+      })
+      .catch(e => {
+        console.error(e);
+        res.send(e);
+      });
   });
 
   //Render the current users maps
   router.get("/favorites/:user_id", (req, res) => {
     // database functions to query for favorite maps of the current user
-
-    res.render("maps");
+    getFavouriteMaps(req.params['user_id'], db)
+    .then(result => {
+      console.log(result)
+      res.render("maps", { maps: result, user: req.session.user_id });
+    })
+    .catch(e => {
+      console.error(e);
+      res.send(e);
+    });
   });
 
-  router.post("/favorites/:user_id", (req, res) => {
+
+  /* POST FAVOUTITES ROUTE UNDER CONSTRUCTION
+    router.post("/favorites/:user_id", (req, res) => {
     //database function to add the current map as a favorite map for the user
-
+    addFavouriteMap(req.params['user_id'], )
     res.redirect("/maps"); //redirect back to the favorites map list
-  });
+  }); */
 
   return router;
 };
