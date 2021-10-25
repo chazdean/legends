a5f14208a1ddd1a375ba27c8f5c92c7a159ee1c3
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-25 19:14:44 +0000

    create a new function for adding a new pin to the database

--- databaseHelpers/pinsQueries.js
+++ databaseHelpers/pinsQueries.js
@@ -42,7 +42,43 @@ const getMapDetails = function(map_id, db) {
 };
 
 
+const addNewPinToMap = function(newPinData, db) {
+  console.log("obj passed into the query", newPinData)
+  const queryParams = [
+    newPinData.title,
+    newPinData.description,
+    newPinData.img_url,
+    newPinData.lat,
+    newPinData.lng,
+    newPinData.map_id,
+    newPinData.creator_id
+  ];
+  const queryString = `
+    INSERT INTO pins (
+      title,
+      description,
+      img_url,
+      lat,
+      lng,
+      map_id,
+      creator_id,
+      date_created
+    )
+    VALUES
+      ($1, $2, $3, $4, $5, $6, $7, now()::date)
+    RETURNING *;`;
+
+  return db.query(queryString, queryParams)
+    .then(result => {
+      return result.rows;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+};
+
 module.exports = {
   getPinsForMap,
-  getMapDetails
+  getMapDetails,
+  addNewPinToMap
 };
