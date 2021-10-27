88a6caa847ae20c3c5a9fee5d2486590994ec908
Justin Wheeler <justinwwheeler@gmail.com>
2021-10-27 02:18:09 +0000

    updated a couple of redirects

--- routes/map.js
+++ routes/map.js
@@ -6,13 +6,10 @@ const express = require('express');
 const router  = express.Router();
 const {
   getMap,
-  addMap,
   updateMap,
   deleteMap
 } = require('../databaseHelpers/mapQueries')
-const {
-  getUserMaps
-} = require('../databaseHelpers/mapsQueries')
+
 
 module.exports = (db) => {
 
@@ -20,7 +17,6 @@ module.exports = (db) => {
   router.get("/:map_id", (req, res) => {
     getMap(req.params['map_id'], db)
       .then(result => {
-        console.log(result)
         res.render("updateMap", { map: result, user: req.session.user_id })
       })
       .catch(e => {
@@ -31,19 +27,11 @@ module.exports = (db) => {
 
   //Update details about map to database on form submit
   router.post("/:map_id", (req, res) => {
+    user_id = req.session.user_id;
 
     updateMap(req.params['map_id'], req.body, db)
       .then(result => {
-        console.log(result)
-        //res.redirect(`../maps/${req.params['map_id']}`);
-        //res.render("maps", { map: result })
-
-        getUserMaps(req.session.user_id, db)
-        .then(result => {
-          console.log(result)
-          res.render("myMaps", { maps: result, user: req.session.user_id });
-        });
-
+        res.redirect(`../../maps/${user_id}`);
       })
       .catch(e => {
         console.error(e);
@@ -53,16 +41,11 @@ module.exports = (db) => {
 
   //Delete map from database on button press
   router.post("/delete/:map_id", (req, res) => {
+    user_id = req.session.user_id;
+
     deleteMap(req.params['map_id'], db)
       .then(result => {
-        console.log(result)
-
-        getUserMaps(req.session.user_id, db)
-        .then(result => {
-          console.log(result)
-          res.render("myMaps", { maps: result, user: req.session.user_id });
-        });
-
+        res.redirect(`../../maps/${user_id}`);
       })
       .catch(e => {
         console.error(e);
